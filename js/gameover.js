132736ecdd2a1891900ee2b27f82da3b5061911f
Unknown <namdang77@hotmail.com>
2018-05-10 12:11:18 -0700

    split evas pages

---
+++ js/gameover.js
@@ -0,0 +1,9 @@
+var gameover = {
+    create: function() {
+        game.add.text(100, 100, "GAMEOVER", {
+                    font: "75px Arial",
+                    fill: "#ffffff",
+                    align: "center"
+        });
+    }
+};
